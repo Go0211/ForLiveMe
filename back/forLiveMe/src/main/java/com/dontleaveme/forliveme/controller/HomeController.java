35ab07ab45279d0c09e0306e06dffcee588763f0
고성태 <v12rhtjdxo@naver.com>
2023-02-01 19:59:02 +0900

    230201_css수정

--- back/forLiveMe/src/main/java/com/dontleaveme/forliveme/controller/HomeController.java
+++ back/forLiveMe/src/main/java/com/dontleaveme/forliveme/controller/HomeController.java
@@ -6,8 +6,12 @@ import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
 import javax.servlet.http.HttpSession;
+import java.util.ArrayList;
+
 @Log4j2
 @Controller
 @RequiredArgsConstructor
@@ -22,5 +26,24 @@ public class HomeController {
         }
 
         return "index";
+    }
+
+    @GetMapping({"/simpleTest"})
+    public String simpleTestStart(Model model, Authentication authentication) {
+        log.info("testStart");
+
+        model.addAttribute("userInfo", authentication.getName());
+        model.addAttribute("test", new ArrayList<String>());
+
+        return "/simpleTest/testPage";
+    }
+
+    @PostMapping({"/simpleTest"})
+    public String simpleTestPost(Model model, Authentication authentication,
+                                 @ModelAttribute("test") ArrayList<String> test) {
+        log.info(test.get(0));
+        log.info("testFinish");
+
+        return "/simpleTest/testPage";
     }
 }
